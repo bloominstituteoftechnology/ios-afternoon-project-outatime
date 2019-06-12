e6ad8a9ba6d6bdd5298bdcf7346c099b025f6e3d
shillwil <alex.shillingford@icloud.com>
2019-06-12 15:06:09 -0600

    Storyboard Tasks completed

---
+++ outatime-ios-afternoon/outatime-ios-afternoon/DatePickerViewController.swift
@@ -0,0 +1,31 @@
+//
+//  DatePickerViewController.swift
+//  outatime-ios-afternoon
+//
+//  Created by Alex Shillingford on 6/12/19.
+//  Copyright © 2019 Alex Shillingford. All rights reserved.
+//
+
+import UIKit
+
+class DatePickerViewController: UIViewController {
+    
+    @IBOutlet weak var datePickerOutlet: UIDatePicker!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func cancelButtonPressed(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func doneButtonPressed(_ sender: UIButton) {
+        
+    }
+    
+    
+}
---
+++ outatime-ios-afternoon/outatime-ios-afternoon/TimeCircuitsViewController.swift
@@ -0,0 +1,31 @@
+//
+//  TimeCircuitsViewController.swift
+//  outatime-ios-afternoon
+//
+//  Created by Alex Shillingford on 6/12/19.
+//  Copyright © 2019 Alex Shillingford. All rights reserved.
+//
+
+import UIKit
+
+class TimeCircuitsViewController: UIViewController {
+    
+    @IBOutlet weak var destinationTimeLabel: UILabel!
+    @IBOutlet weak var presentTimeLabel: UILabel!
+    @IBOutlet weak var lastTimeDepartedLabel: UILabel!
+    @IBOutlet weak var speedLabel: UILabel!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func travelBackButtonPressed(_ sender: Any) {
+        
+    }
+    
+    
+    
+}
