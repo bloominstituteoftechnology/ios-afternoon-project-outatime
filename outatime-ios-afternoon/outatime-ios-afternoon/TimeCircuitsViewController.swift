9ac8caa823cb030333521ec82b889388c0ca2bec
shillwil <alex.shillingford@icloud.com>
2019-06-12 23:16:11 -0600

    MVP achieved

--- outatime-ios-afternoon/outatime-ios-afternoon/TimeCircuitsViewController.swift
+++ outatime-ios-afternoon/outatime-ios-afternoon/TimeCircuitsViewController.swift
@@ -10,13 +10,14 @@ import UIKit
 
 class TimeCircuitsViewController: UIViewController {
     
+    // MARK: - Outlets and properties
     @IBOutlet weak var destinationTimeLabel: UILabel!
     @IBOutlet weak var presentTimeLabel: UILabel!
     @IBOutlet weak var lastTimeDepartedLabel: UILabel!
     @IBOutlet weak var speedLabel: UILabel!
     
     var currentSpeed: Double = 0.0
-    var timer: Timer?
+    var timer = Timer()
     var datePickerVC = DatePickerViewController()
     
     var dateFormatter: DateFormatter = {
@@ -31,11 +32,17 @@ class TimeCircuitsViewController: UIViewController {
 
         presentTimeLabel.text = dateFormatter.string(from: Date())
     }
-    
+    // MARK: - IBActions, Methods, and Functions
     @IBAction func travelBackButtonPressed(_ sender: Any) {
-        func startTimer() {
-            
-        }
+        startTimer()
+    }
+    
+    func startTimer() {
+        timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(updateSpeed), userInfo: nil, repeats: true)
+    }
+    
+    func resetTimer() {
+        timer.invalidate()
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -43,11 +50,22 @@ class TimeCircuitsViewController: UIViewController {
         datePickerVC.delegate = self
     }
     
-    func updateViews() {
-        
+    // MARK: - Objective-C Functions
+    @objc func updateSpeed() {
+        if currentSpeed < 88 {
+            currentSpeed += 1
+            speedLabel.text = "\(currentSpeed) MPH"
+        } else {
+            speedLabel.text = "\(currentSpeed) MPH"
+            currentSpeed = 0
+            lastTimeDepartedLabel.text = presentTimeLabel.text
+            presentTimeLabel.text = destinationTimeLabel.text
+            resetTimer()
+        }
     }
 }
 
+// MARK: - Extensions
 extension TimeCircuitsViewController: DatePickerDelegate {
     func destinationDateWasChosen(_ destinationDate: Date) {
         destinationTimeLabel.text = dateFormatter.string(from: destinationDate)
