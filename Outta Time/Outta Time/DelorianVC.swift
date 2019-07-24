b4a3c9c56333006f5459029295ce7f83d0d2e7c1
Jeffrey Santana <SantanaJny@gmail.com>
2019-07-24 16:18:55 -0400

    Storyboard complete and linked

---
+++ Outta Time/Outta Time/DelorianVC.swift
@@ -0,0 +1,29 @@
+//
+//  DelorianVC.swift
+//  Outta Time
+//
+//  Created by Jeffrey Santana on 7/24/19.
+//  Copyright © 2019 Jefffrey Santana. All rights reserved.
+//
+
+import UIKit
+
+class DelorianVC: UIViewController {
+
+	@IBOutlet weak var destinationLbl: UILabel!
+	@IBOutlet weak var presentLbl: UILabel!
+	@IBOutlet weak var previousLbl: UILabel!
+	@IBOutlet weak var speedLbl: UILabel!
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+	@IBAction func setDestinationBtnAction(_ sender: Any) {
+	}
+	
+	@IBAction func travelBtnAction(_ sender: Any) {
+	}
+}
+
