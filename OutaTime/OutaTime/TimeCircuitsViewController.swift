9a1309f4f789bc1f9706a97bce31f8cd7528b4f1
JerryHaaser <jerry.haaser@hotmail.com>
2019-07-24 15:47:04 -0500

    added destination time, present time, last time departed, and speed outlets, as well as set destination time and travel back actions

--- OutaTime/OutaTime/TimeCircuitsViewController.swift
+++ OutaTime/OutaTime/TimeCircuitsViewController.swift
@@ -9,11 +9,24 @@
 import UIKit
 
 class TimeCircuitsViewController: UIViewController {
+    
+    @IBOutlet weak var destinationTimeLabel: UILabel!
+    @IBOutlet weak var presentTimeLabel: UILabel!
+    @IBOutlet weak var lastTimeDepartedLabel: UILabel!
+    @IBOutlet weak var speedLabel: UILabel!
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func setDestinationTimeButtonPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func travelBackButtonPressed(_ sender: Any) {
     }
     
 
