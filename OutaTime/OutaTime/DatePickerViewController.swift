3d2481c3a7fc7d486ad93fdb8b2ed9808a623cd2
Blake Andrew Price <blakeandrewprice@gmail.com>
2019-07-24 14:08:31 -0700

    Storyboard task complete, build runs

---
+++ OutaTime/OutaTime/DatePickerViewController.swift
@@ -0,0 +1,22 @@
+//
+//  DatePickerViewController.swift
+//  OutaTime
+//
+//  Created by Blake Andrew Price on 7/24/19.
+//  Copyright © 2019 Blake Andrew Price. All rights reserved.
+//
+
+import UIKit
+
+class DatePickerViewController: UIViewController {
+
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBAction func cancelButtonTapped(_ sender: UIBarButtonItem) {
+    }
+    @IBAction func doneButtonTapped(_ sender: UIBarButtonItem) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
---
+++ OutaTime/OutaTime/TimeCircuitsViewController.swift
@@ -0,0 +1,22 @@
+//
+//  TimeCircuitsViewController.swift
+//  OutaTime
+//
+//  Created by Blake Andrew Price on 7/24/19.
+//  Copyright © 2019 Blake Andrew Price. All rights reserved.
+//
+
+import UIKit
+
+class TimeCircuitsViewController: UIViewController {
+    @IBOutlet weak var destinationTimeLabel: UILabel!
+    @IBOutlet weak var presentTime: UILabel!
+    @IBOutlet weak var lastTimeDepartedLabel: UILabel!
+    @IBOutlet weak var speedMPHLabel: UILabel!
+    @IBAction func travelBackButtonTapped(_ sender: UIButton) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
