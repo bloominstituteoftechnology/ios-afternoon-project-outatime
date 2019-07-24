b7bd0d19c1d4e00928b1ac112259dfcb9bdae7dc
bradleyyin <bradleyyin@gmail.com>
2019-07-24 13:06:51 -0700

    basic UI setup

---
+++ OutaTime/OutaTime/DatePickerViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DatePickerViewController.swift
+//  OutaTime
+//
+//  Created by Bradley Yin on 7/24/19.
+//  Copyright © 2019 bradleyyin. All rights reserved.
+//
+
+import UIKit
+
+class DatePickerViewController: UIViewController {
+    
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func doneButtonTapped(_ sender: UIBarButtonItem) {
+    }
+    
+    @IBAction func cancelButtonTapped(_ sender: UIBarButtonItem) {
+    }
+    
+   
+
+}
---
+++ OutaTime/OutaTime/TimeCircuitsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TimeCircuitsViewController.swift
+//  OutaTime
+//
+//  Created by Bradley Yin on 7/24/19.
+//  Copyright © 2019 bradleyyin. All rights reserved.
+//
+
+import UIKit
+
+class TimeCircuitsViewController: UIViewController {
+    
+    @IBOutlet weak var destinationLabel: UILabel!
+    @IBOutlet weak var presentLabel: UILabel!
+    @IBOutlet weak var lastLabel: UILabel!
+    @IBOutlet weak var sppedLabel: UILabel!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func travelButtonTapped(_ sender: UIButton) {
+    }
+    
+
+}
