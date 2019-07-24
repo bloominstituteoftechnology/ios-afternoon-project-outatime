628948bcc293747b9d208bc03701e9b8278326bb
Andrew Ruiz <nukedoom@gmail.com>
2019-07-24 14:27:57 -0700

    Completed date formatter.

--- Outatime/TimeCircuitsViewController.swift
+++ Outatime/TimeCircuitsViewController.swift
@@ -11,18 +11,37 @@ import UIKit
 class TimeCircuitsViewController: UIViewController {
 
     // Outlets
-    
     @IBOutlet weak var destinationTime: UILabel!
     @IBOutlet weak var presentTime: UILabel!
     @IBOutlet weak var lastTimeDeparted: UILabel!
     @IBOutlet weak var speed: UILabel!
     @IBOutlet weak var setDestinationTime: UIButton!
     
+    // Properties
+    var currentSpeed: Double = 0
+    
+    // Date Formatter
+    var dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "EEE, MMM d, ''yy"
+        formatter.timeZone = TimeZone(secondsFromGMT: 0)
+        return formatter
+    }()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Set current date/time
+        let currentTime = dateFormatter.string(from: Date())
+        presentTime.text = currentTime
+        
+        // Set the speed
+        speed.text = "\(String(currentSpeed)) mph)"
+        
+        // Set the last time departed
+        lastTimeDeparted.text = "--- -- ----"
 
-        // Do any additional setup after loading the view.
     }
     
     
