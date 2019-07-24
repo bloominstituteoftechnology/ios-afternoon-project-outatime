89ad61ba369f52c3f0101d620dca55ab2455a83e
Jeffrey Santana <SantanaJny@gmail.com>
2019-07-24 16:40:21 -0400

    Passing date via protocol and shifting all date labels

--- Outta Time/Outta Time/DelorianVC.swift
+++ Outta Time/Outta Time/DelorianVC.swift
@@ -15,15 +15,41 @@ class DelorianVC: UIViewController {
 	@IBOutlet weak var previousLbl: UILabel!
 	@IBOutlet weak var speedLbl: UILabel!
 	
+	private var dateFormatter: DateFormatter {
+		let formatter = DateFormatter()
+		formatter.dateStyle = .medium
+		return formatter
+	}
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view.
+		
+		startDelorian()
 	}
-
-	@IBAction func setDestinationBtnAction(_ sender: Any) {
+	
+	override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+		if let datePickerVC = segue.destination as? DatePickerVC {
+			datePickerVC.delegate = self
+		}
 	}
 	
 	@IBAction func travelBtnAction(_ sender: Any) {
+		shiftDates()
+	}
+	
+	private func startDelorian() {
+		presentLbl.text = dateFormatter.string(from: Date())
+	}
+	
+	private func shiftDates() {
+		previousLbl.text = presentLbl.text
+		presentLbl.text = destinationLbl.text
+		destinationLbl.text = "--- -- ----"
 	}
 }
 
+extension DelorianVC: DatePickerVCDelegate {
+	func set(time: Date) {
+		destinationLbl.text = dateFormatter.string(from: time)
+	}
+}
