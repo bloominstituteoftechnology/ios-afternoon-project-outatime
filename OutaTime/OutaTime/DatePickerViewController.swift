6472f0d5418e00ec858732a87cd82832d0766f64
Alex Rhodes <anrhodes5738@gmail.com>
2019-07-24 16:01:20 -0500

    Set up storyboard.

---
+++ OutaTime/OutaTime/DatePickerViewController.swift
@@ -0,0 +1,21 @@
+//
+//  DatePickerViewController.swift
+//  OutaTime
+//
+//  Created by Alex Rhodes on 7/24/19.
+//  Copyright © 2019 Alex Rhodes. All rights reserved.
+//
+
+import UIKit
+
+class DatePickerViewController: UIViewController {
+
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    
+    @IBAction func cancelButtoonTapped(_ sender: UIBarButtonItem) {
+    }
+    
+    @IBAction func doneButtonTapped(_ sender: UIBarButtonItem) {
+    }
+}
---
+++ OutaTime/OutaTime/TimeViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  OutaTime
+//
+//  Created by Alex Rhodes on 7/24/19.
+//  Copyright © 2019 Alex Rhodes. All rights reserved.
+//
+
+import UIKit
+
+class TimeViewController: UIViewController {
+
+    @IBOutlet weak var destinationTimeLabel: UILabel!
+    @IBOutlet weak var presentTimeLabel: UILabel!
+    @IBOutlet weak var lastTimeDepartedLabel: UILabel!
+    @IBOutlet weak var speedLabel: UILabel!
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    @IBAction func setDestinationTimeButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func travelBackButton(_ sender: UIButton) {
+    }
+    
+
+}
+
