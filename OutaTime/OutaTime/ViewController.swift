de72b8c3fa5b70cda8ebfcec1035c7685069eda8
ry <xxsnailyxx@gmai.com>
2019-07-25 16:07:02 -0700

    mvp outta time

---
+++ OutaTime/OutaTime/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  OutaTime
+//  Created by Ry Bautista on 7/24/19.
+//  Copyright © 2019 Ry Bautista All rights reserved.
+//
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
