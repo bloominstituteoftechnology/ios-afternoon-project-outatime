12adff95908631e00dc15f39d9ae871c36befc4d
John Kouris <jdkouris@gmail.com>
2019-08-11 21:05:52 -0700

    Initial attempt at completing all the instructions, not working yet

--- OutaTime/OutaTime/DatePickerViewController.swift
+++ OutaTime/OutaTime/DatePickerViewController.swift
@@ -8,23 +8,27 @@
 
 import UIKit
 
+protocol DatePickerDelegate {
+    func destinationDateWasChosen(forDate date: Date)
+}
+
 class DatePickerViewController: UIViewController {
     
     @IBOutlet weak var datePicker: UIDatePicker!
     
-
+    var delegate: DatePickerDelegate?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
     @IBAction func cancelButtonTapped(_ sender: Any) {
-        
+        dismiss(animated: true, completion: nil)
     }
     
     @IBAction func saveButtonTapped(_ sender: Any) {
-        
+        delegate?.destinationDateWasChosen(forDate: datePicker.date)
+        dismiss(animated: true, completion: nil)
     }
     
 
--- OutaTime/OutaTime/TimeCircuitsViewController.swift
+++ OutaTime/OutaTime/TimeCircuitsViewController.swift
@@ -15,16 +15,69 @@ class TimeCircuitsViewController: UIViewController {
     @IBOutlet weak var lastTimeDepartedLabel: UILabel!
     @IBOutlet weak var speedLabel: UILabel!
     
+    var dateFormatter: DateFormatter {
+        self.dateFormatter.dateStyle = .medium
+        return self.dateFormatter
+    }
+    
+    var currentSpeed = 0
+    
+    let currentDate = Date()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         
+        presentTimeLabel.text = dateFormatter.string(from: currentDate)
+        speedLabel.text = "\(currentSpeed) MPH"
+        lastTimeDepartedLabel.text = "--- -- ----"
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "ModalDestinationDatePickerSegue" {
+            guard let destinationVC = segue.destination as? DatePickerViewController else { return }
+            destinationVC.delegate = self
+        }
     }
     
     @IBAction func travelBackButtonTapped(_ sender: Any) {
+        startTimer()
+    }
+    
+    func startTimer() {
+        let timer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: false) { (timer) in
+            self.updateSpeed()
+        }
+    }
+    
+    func resetTimer() {
         
     }
     
+    func updateSpeed() {
+        if currentSpeed == 88 {
+            resetTimer()
+            lastTimeDepartedLabel.text = dateFormatter.string(from: currentDate)
+            presentTimeLabel.text = destinationTimeLabel.text
+            currentSpeed = 0
+            presentAlert()
+        } else {
+            currentSpeed += 1
+            speedLabel.text = "\(currentSpeed) MPH"
+        }
+    }
+    
+    func presentAlert() {
+        let alert = UIAlertController(title: "Time Travel Successful", message: "You're new date is \(destinationTimeLabel.text)", preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(okAction)
+        present(alert, animated: true, completion: nil)
+    }
+    
 
+}
+
+extension TimeCircuitsViewController: DatePickerDelegate {
+    func destinationDateWasChosen(forDate date: Date) {
+        destinationTimeLabel.text = dateFormatter.string(from: date)
+    }
 }
