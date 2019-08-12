52f3909339d092d65dab879d99a94ce7f4cdfcf7
John Kouris <jdkouris@gmail.com>
2019-08-11 20:30:17 -0700

    Added the DatePickerViewController and TimeCircuitsViewController Designed the UI in storyboard Wired up the necessary outlets and actions

---
+++ OutaTime/OutaTime/DatePickerViewController.swift
@@ -0,0 +1,31 @@
+//
+//  DatePickerViewController.swift
+//  OutaTime
+//
+//  Created by John Kouris on 8/11/19.
+//  Copyright © 2019 John Kouris. All rights reserved.
+//
+
+import UIKit
+
+class DatePickerViewController: UIViewController {
+    
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func cancelButtonTapped(_ sender: Any) {
+        
+    }
+    
+    @IBAction func saveButtonTapped(_ sender: Any) {
+        
+    }
+    
+
+}
---
+++ OutaTime/OutaTime/TimeCircuitsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TimeCircuitsViewController.swift
+//  OutaTime
+//
+//  Created by John Kouris on 8/11/19.
+//  Copyright © 2019 John Kouris. All rights reserved.
+//
+
+import UIKit
+
+class TimeCircuitsViewController: UIViewController {
+    
+    @IBOutlet weak var destinationTimeLabel: UILabel!
+    @IBOutlet weak var presentTimeLabel: UILabel!
+    @IBOutlet weak var lastTimeDepartedLabel: UILabel!
+    @IBOutlet weak var speedLabel: UILabel!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    @IBAction func travelBackButtonTapped(_ sender: Any) {
+        
+    }
+    
+
+}
