a0b60dd03897c85fed3a94f31f5ac3c8f0d7f0c4
Vici Shaweddy <vshaweddy@gmail.com>
2019-08-13 14:22:10 -0400

    storyboard wip

---
+++ Outatime/Outatime/TimeCircuitsViewController.swift
@@ -0,0 +1,40 @@
+//
+//  TimeCircuitsViewController.swift
+//  OutaTime
+//
+//  Created by Vici Shaweddy on 8/13/19.
+//  Copyright © 2019 Vici Shaweddy. All rights reserved.
+//
+
+import UIKit
+
+class TimeCircuitsViewController: UIViewController {
+    
+    @IBOutlet weak var destinationTimeLabel: UILabel!
+    @IBOutlet weak var presentTimeLabel: UILabel!
+    @IBOutlet weak var lastTimeDepartedLabel: UILabel!
+    @IBOutlet weak var speedLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func travelBackButton(_ sender: Any) {
+    }
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
