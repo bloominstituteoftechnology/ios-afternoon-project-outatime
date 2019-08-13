edc179f29105692eabc6feddae4b0d289d30a4ac
Jessie Griffin <i.jessie.me@icloud.com>
2019-08-12 23:55:29 -0400

    TimeCircuitsView UI Design Complete

---
+++ OutaTime/OutaTime/TimeCircuitsViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OutaTime
+//
+//  Created by Jessie Ann Griffin on 8/12/19.
+//  Copyright © 2019 Jessie Griffin. All rights reserved.
+//
+
+import UIKit
+
+class TimeCircuitsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
