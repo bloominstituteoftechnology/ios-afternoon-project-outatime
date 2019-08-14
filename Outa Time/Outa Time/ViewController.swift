7ae112c033e192f31390eebf4ab90589348ee092
dillonp23 <dillonp.lambda@gmail.com>
2019-08-13 18:18:47 -0700

    Initial commit.

---
+++ Outa Time/Outa Time/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Outa Time
+//
+//  Created by Dillon P on 8/13/19.
+//  Copyright © 2019 Dillon P. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
