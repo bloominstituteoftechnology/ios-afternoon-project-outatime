8167f386e5d9f902cff49b1b9ccea5e408e97462
Jessie Griffin <i.jessie.me@icloud.com>
2019-08-13 21:37:03 -0400

    Completed first Code tasks for TimeCircuitsViewController

--- OutaTime/OutaTime/TimeCircuitsViewController.swift
+++ OutaTime/OutaTime/TimeCircuitsViewController.swift
@@ -10,15 +10,27 @@ import UIKit
 
 class TimeCircuitsViewController: UIViewController {
 
-    
+    // MARK: Properties
     @IBOutlet weak var destinationTimeLabel: UILabel!
     @IBOutlet weak var presentTimeLabel: UILabel!
     @IBOutlet weak var lastTimeLabel: UILabel!
     @IBOutlet weak var speedLabel: UILabel!
     
+    var dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "MMM dd yyyy"
+        formatter.timeZone = TimeZone(secondsFromGMT: 0)
+        return formatter
+    }()
+    
+    var speed = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        //let presentDay = Date()
+        presentTimeLabel.text = dateFormatter.string(from: Date())
+        speedLabel.text = "\(speed) MPH"
+        lastTimeLabel.text = "--- -- ----"
     }
 
     /*
