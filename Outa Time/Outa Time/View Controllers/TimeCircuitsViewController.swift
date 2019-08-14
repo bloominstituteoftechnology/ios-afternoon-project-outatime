9ef0ccec63256ac7431f203b196d20451c31b9f2
dillonp23 <dillonp.lambda@gmail.com>
2019-08-13 19:20:24 -0700

    Add code to Time View Controller. Wire up outlets and actions. Add  modal segue to date picker.

---
+++ Outa Time/Outa Time/View Controllers/TimeCircuitsViewController.swift
@@ -0,0 +1,53 @@
+//
+//  TimeCircuitsViewController.swift
+//  Outa Time
+//
+//  Created by Dillon P on 8/13/19.
+//  Copyright © 2019 Dillon P. All rights reserved.
+//
+
+import UIKit
+
+class TimeCircuitsViewController: UIViewController {
+
+    @IBOutlet weak var destinationTimeLabel: UILabel!
+    @IBOutlet weak var presentTimeLabel: UILabel!
+    @IBOutlet weak var lastTimeDepartedLabel: UILabel!
+    @IBOutlet weak var speedLabel: UILabel!
+    
+    
+    var dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "MMM dd yyyy"
+        formatter.timeZone = TimeZone(secondsFromGMT: -25200)
+        return formatter
+    }()
+    
+    var currentSpeed = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        presentTimeLabel.text = dateFormatter.string(from: Date())
+        speedLabel.text = "\(currentSpeed) MPH"
+        lastTimeDepartedLabel.text = "--- -- ----"
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    
+    @IBAction func travelBackTapped(_ sender: Any) {
+    }
+    
+    
+    
+
+}
