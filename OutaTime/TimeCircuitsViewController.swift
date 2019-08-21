3b468850c63e90f5ed33992e254edfe0abf4dedb
AustinPotts <pottsevolvements@gmail.com>
2019-08-21 11:47:27 -0400

    Storyboard Edit Added Proper Labels
    Added proper labels for the time display & font.

--- OutaTime/TimeCircuitsViewController.swift
+++ OutaTime/TimeCircuitsViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class TimeCircuitsViewController: UIViewController {
 
+    
+    @IBAction func travelBackTapped(_ sender: Any) {
+    }
+    
+    @IBAction func setDestinationTime(_ sender: Any) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -17,14 +24,6 @@ class TimeCircuitsViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+   
 
 }
