bb7e2ad25e26bf0fc4d9b66e350288f6e9968c02
ladybeitel <ciarabeitel@gmail.com>
2019-08-21 17:48:00 -0400

    Complete Code Tasks: create computed property for date formatter set present time to current date set current speed set last time departed declare protocol, declare delegate create extension in TimeCircuitsViewController

--- OutaTime/OutaTime/View Controllers/DatePickerViewController.swift
+++ OutaTime/OutaTime/View Controllers/DatePickerViewController.swift
@@ -8,6 +8,10 @@
 
 import UIKit
 
+protocol DatePickerDelegate {
+    func destinationDateWasChosen(_: Date)
+}
+
 class DatePickerViewController: UIViewController {
     
     @IBOutlet weak var datePicker: UIDatePicker!
@@ -18,21 +22,10 @@ class DatePickerViewController: UIViewController {
     @IBAction func doneTapped(_ sender: Any) {
     }
     
+    var delegate: DatePickerDelegate?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
--- OutaTime/OutaTime/View Controllers/TimeCircuitsViewController.swift
+++ OutaTime/OutaTime/View Controllers/TimeCircuitsViewController.swift
@@ -25,21 +25,28 @@ class TimeCircuitsViewController: UIViewController {
     @IBAction func travelBackTapped(_ sender: Any) {
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    var dateFormatter: DateFormatter {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "MMM dd, yyyy"
+        formatter.timeZone = TimeZone(secondsFromGMT: 0)
+        return formatter
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let date = Date()
+        presentTimeLabel.text = dateFormatter.string(from: date)
+        
+        let currentSpeed = 0
+        speedLabel.text = "\(currentSpeed) MPH"
+        
+        lastTimeDepartedLabel.text = "--- -- ----"
     }
-    */
+}
 
+extension TimeCircuitsViewController: DatePickerDelegate {
+    func destinationDateWasChosen(_: Date) {
+        <#code#>
+    }
 }
