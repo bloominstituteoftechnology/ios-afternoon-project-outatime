0e57526c23df884424cf552c83363c8e1d250425
Andrew Ruiz <nukedoom@gmail.com>
2019-08-21 19:55:19 -0700

    Implemented final changes. Unsure why not working though.

--- Outatime/View Controllers/DatePickerViewController.swift
+++ Outatime/View Controllers/DatePickerViewController.swift
@@ -8,8 +8,15 @@
 
 import UIKit
 
+protocol DatePickerDelegate: AnyObject {
+    func destinationDateWasChosen(date: Date)
+}
+
 class DatePickerViewController: UIViewController {
     
+    @IBOutlet weak var datePicker: UIDatePicker!
+    weak var delegate: DatePickerDelegate?
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -17,14 +24,13 @@ class DatePickerViewController: UIViewController {
     }
     
     
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destination.
-     // Pass the selected object to the new view controller.
-     }
-     */
+    @IBAction func doneButtonPressed(_ sender: Any) {
+        delegate?.destinationDateWasChosen(date: datePicker.date)
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func cancelButtonPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
     
 }
--- Outatime/View Controllers/TimeCircuitsViewController.swift
+++ Outatime/View Controllers/TimeCircuitsViewController.swift
@@ -8,6 +8,12 @@
 
 import UIKit
 
+enum CountdownState {
+    case started
+    case finished
+    case reset
+}
+
 class TimeCircuitsViewController: UIViewController {
 
     @IBOutlet weak var destinationTime: UILabel!
@@ -15,31 +21,86 @@ class TimeCircuitsViewController: UIViewController {
     @IBOutlet weak var lastTimeDeparted: UILabel!
     @IBOutlet weak var speed: UILabel!
     
+    var currentSpeed: Double = 0
     
-    
-    
-    
+    var dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "EEE, MMM d, ''yy"
+        formatter.timeZone = TimeZone(secondsFromGMT: 0)
+        return formatter
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        let currentTime = dateFormatter.string(from: Date())
+        
+        presentTime.text = currentTime
+        speed.text = "\(String(currentSpeed)) mph"
+        lastTimeDeparted.text = "--- -- ----"
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func showAlert() {
+        
+        var unwrappedAlert: String = ""
+        if let unwrappedPresentTime = presentTime.text {
+            unwrappedAlert = unwrappedPresentTime
+        }
+        let alert = UIAlertController(title: "Time travel successful.", message: "Your new date is \(unwrappedAlert).", preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+        self.present(alert, animated: true)
     }
-    */
+    
+    private var timer: Timer?
+    
+    private(set) var state: CountdownState?
+    
+    func resetTimer() {
+        cancelTimer()
+    }
+    
+    func cancelTimer() {
+        timer?.invalidate()
+        timer = nil
+    }
+    
+    func startTimer() {
+        cancelTimer()
+        timer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true, block: updateSpeed(timer:))
+    }
+    
+    func updateSpeed(timer: Timer) {
+        if currentSpeed != 88 {
+            currentSpeed = currentSpeed + 1
+            speed.text = String(currentSpeed) + "mph"
+        } else if currentSpeed == 88 {
+            cancelTimer()
+            lastTimeDeparted.text = presentTime.text
+            presentTime.text = destinationTime.text
+            currentSpeed = 0
+            showAlert()
+        }
+    }
+    
+    
 
     @IBAction func setDestinationTimeButtonPressed(_ sender: Any) {
     }
     
     @IBAction func travelBackButtonPressed(_ sender: Any) {
+        startTimer()
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let destination = segue.destination as? DatePickerViewController {
+            destination.delegate = self
+        }
+    }
+}
+
+extension TimeCircuitsViewController: DatePickerDelegate {
+    func destinationDateWasChosen(date: Date) {
+        destinationTime.text = dateFormatter.string(from: date)
     }
 }
