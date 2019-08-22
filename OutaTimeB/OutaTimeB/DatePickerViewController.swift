f71b31973695af4e48706389d78a91e8d6af8c86
Percy <percival.ngan@gmail.com>
2019-08-22 10:06:04 -0500

    almost complete, but app not working properly

--- OutaTimeB/OutaTimeB/DatePickerViewController.swift
+++ OutaTimeB/OutaTimeB/DatePickerViewController.swift
@@ -8,25 +8,37 @@
 
 import UIKit
 
+protocol DatePickerDelegate {
+	func destinationDateWasChosen(date: Date)
+	}
+
+
 class DatePickerViewController: UIViewController {
 
 
 	@IBOutlet weak var datePicker: UIDatePicker!
-	
+
+
+	var delegate: DatePickerDelegate?
 
 
 	override func viewDidLoad() {
         super.viewDidLoad()
 
+		datePicker.setValue(UIColor.white, forKey: "textColor")
 
     }
 
-	// MARK: = Properties
 
 	@IBAction func cancelButtonPressed(_ sender: UIBarButtonItem) {
+		dismiss(animated: true, completion: nil)
 	}
 	
+
 	@IBAction func doneButtonPressed(_ sender: UIBarButtonItem) {
+		let date = datePicker.date
+		delegate?.destinationDateWasChosen(date: date)
+		dismiss(animated: true, completion: nil)
 	}
 	
 
--- OutaTimeB/OutaTimeB/TimeCircuitsViewController.swift
+++ OutaTimeB/OutaTimeB/TimeCircuitsViewController.swift
@@ -12,34 +12,44 @@ class TimeCircuitsViewController: UIViewController {
 
 
 
+	@IBOutlet weak var destinationLabel: UILabel!
 	@IBOutlet weak var destinationTimeLabel: UILabel!
 	@IBOutlet weak var presentDate: UILabel!
 	@IBOutlet weak var lastTimeDepartedLabel: UILabel!
-	@IBOutlet weak var speedLabel: UILabel!
+	@IBOutlet weak var lastTimeLabel: UILabel!
 	@IBOutlet weak var presentTimeLabel: UILabel!
+	@IBOutlet weak var setDestinationTimeButton: UIButton!
+	@IBOutlet weak var currentSpeedLabel: UILabel!
+	@IBOutlet weak var speedLabel: UILabel!
 
+	
 	var dateFormatter: DateFormatter = {
+
 		let formatter = DateFormatter()
 		formatter.dateFormat = "MMM dd yyyy"
+		formatter.timeZone = TimeZone(secondsFromGMT: 0)
 		return formatter
 	}()
 
-	var currentSpeed: Int = 100
 
+	var currentSpeed: Int = 0
+	var timer: Timer?
 
 	
-
-
 
 	override func viewDidLoad() {
         super.viewDidLoad()
 
 		view.backgroundColor = .black
-		destinationTimeLabel.textColor = .white
+		destinationLabel.textColor = .white
+		destinationTimeLabel.textColor = .orange
 		presentTimeLabel.textColor = .white
 		presentDate.textColor = .green
-		lastTimeDepartedLabel.textColor = .white
+		lastTimeDepartedLabel.textColor = .yellow
+		lastTimeLabel.textColor = .white
+		currentSpeedLabel.textColor = .red
 		speedLabel.textColor = .white
+
 
 		// sets the presentDate to today's date
 		let currentDate = Date()
@@ -50,10 +60,61 @@ class TimeCircuitsViewController: UIViewController {
 		speedLabel.text = String("\(currentSpeed) MPH")
 		lastTimeDepartedLabel.text = "--- -- ----"
 
+
     }
+
+
     
 
 	@IBAction func travelBackButton(_ sender: UIButton) {
+
+		startTimer()
+
 	}
 
+
+	override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+		if segue.identifier == "ModalDestinationDatePickerSegue" {
+			guard let datePickerVC = segue.destination as? DatePickerViewController else { return }
+			datePickerVC.delegate = self
+	}
+}
+
+
+	func resetTimer() {
+		timer?.invalidate()
+		timer = nil
+	}
+
+	func updateSpeed(timer: Timer) {
+		if currentSpeed < 88 {
+			currentSpeed += 1
+			currentSpeedLabel.text = String(currentSpeed)
+		} else {
+			resetTimer()
+			lastTimeDepartedLabel.text = presentTimeLabel.text
+			presentTimeLabel.text = destinationTimeLabel.text
+			currentSpeed = 0
+
+			showAlert()
+		}
+	}
+
+	func showAlert() {
+		let alert = UIAlertController(title: "Time Travel Successful", message: "You're new date is \(presentTimeLabel.text ?? "")", preferredStyle: .alert)
+		alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+		present(alert, animated: true, completion: nil)
+	}
+
+	func startTimer() {
+		timer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true, block: updateSpeed(timer:))
+	}
+}
+
+extension TimeCircuitsViewController: DatePickerDelegate {
+	func destinationDateWasChosen(date: Date) {
+
+		let date = dateFormatter.string(from: date)
+		destinationTimeLabel.text = date
+	}
 }
