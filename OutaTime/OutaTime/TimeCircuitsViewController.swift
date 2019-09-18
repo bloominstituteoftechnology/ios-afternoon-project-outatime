bfa3100675c19d0c833df1cacb0dbf38542afebc
Jerry Haaser <jerry.haaser@hotmail.com>
2019-09-18 14:54:57 -0500

    created TimeCircuitsViewController and DatePickerVIewController also set up TimeCircuits UI and conttect outlets and actions

---
+++ OutaTime/OutaTime/TimeCircuitsViewController.swift
@@ -0,0 +1,44 @@
+//
+//  TimeCircuitsViewController.swift
+//  OutaTime
+//
+//  Created by admin on 9/18/19.
+//  Copyright © 2019 admin. All rights reserved.
+//
+
+import UIKit
+
+class TimeCircuitsViewController: UIViewController {
+
+    @IBOutlet weak var destinationLabel: UILabel!
+    @IBOutlet weak var presentLabel: UILabel!
+    @IBOutlet weak var lastLabel: UILabel!
+    @IBOutlet weak var speedLabel: UILabel!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func setDestinationButton(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func travelBackButton(_ sender: UIButton) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
