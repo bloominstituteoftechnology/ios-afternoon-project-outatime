9603692e5340a006e4d581ea5639ff08d6de98b7
Dennis <dpr1996@gmail.com>
2019-09-18 19:36:15 -0400

    Almost done

---
+++ OutaTime/OutaTime/DatePickerViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DatePickerViewController.swift
+//  OutaTime
+//
+//  Created by Dennis on 9/18/19.
+//  Copyright © 2019 LambdaSchool. All rights reserved.
+//
+
+import UIKit
+
+protocol DatePickerDelegate {
+    func destinationDateWasChosen(date: Date)
+}
+
+class DatePickerViewController: UIViewController {
+    
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    var delegate: DatePickerDelegate?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+   
+    @IBAction func cancelButtonTapped(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func doneButtonTapped(_ sender: Any) {
+        delegate?.destinationDateWasChosen(date: datePicker.date)
+        dismiss(animated: true, completion: nil)
+    }
+
+}
