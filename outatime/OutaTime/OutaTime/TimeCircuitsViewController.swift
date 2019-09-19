bac5bc5731ca085f05ce06deda1ae951d4611236
RBlume <54851947+RBlume@users.noreply.github.com>
2019-09-19 18:35:07 -0400

    Add files via upload
    final

---
+++ outatime/OutaTime/OutaTime/TimeCircuitsViewController.swift
@@ -0,0 +1,98 @@
+//
+//  TimeCircuitsViewController.swift
+//  OutaTime
+//
+///  Created by Robert B on 9/19/19.
+//  Copyright © 2019 lambda school. All rights reserved.
+//
+
+
+import UIKit
+
+class TimeCircuitsViewController: UIViewController {
+
+    @IBOutlet weak var destinationLabel: UILabel!
+    @IBOutlet weak var presentLabel: UILabel!
+    @IBOutlet weak var lastLabel: UILabel!
+    @IBOutlet weak var speedLabel: UILabel!
+    
+    let currentDate = Date()
+    
+    var currentSpeed = 0
+    
+    var timer: Timer?
+    
+    var dateFormatter: DateFormatter  {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "MMM d yyyy"
+        formatter.timeZone = TimeZone(secondsFromGMT: 0)
+        return formatter
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        guard UIFont(name: "Digital-7", size: UIFont.labelFontSize) != nil else { return }
+        
+        presentLabel.text = dateFormatter.string(from: currentDate)
+        //presentLabel.font = UIFontMetrics.default.scaledFont(for: customFont)
+        speedLabel.text = "\(currentSpeed) MPH"
+        //speedLabel.font = UIFontMetrics.default.scaledFont(for: customFont)
+        lastLabel.text = "___ __ ____"
+        //lastLabel.font = UIFontMetrics.default.scaledFont(for: customFont)
+
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func travelBackButton(_ sender: UIButton) {
+        startTimer()
+    }
+    
+    func startTimer() {
+        timer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true, block: updateSpeed(timer:))
+    }
+    
+    func resetTimer() {
+        timer?.invalidate()
+        timer = nil
+    }
+    
+    func updateSpeed(timer: Timer) {
+        if currentSpeed < 88 {
+            currentSpeed += 1
+            speedLabel.text = "\(currentSpeed) MPH"
+        } else {
+            resetTimer()
+            guard let presentTime = presentLabel.text else { return }
+            lastLabel.text = presentLabel.text
+            currentSpeed = 0
+            let alert = UIAlertController(title: "Time Travel Successful!", message: "Your new date is \(presentTime)", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            present(alert, animated: true, completion: nil)
+        }
+    }
+    
+    
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "ModalDestinationDatePickerSegue" {
+            if let datePickerVC = segue.destination as? DatPickerViewController {
+                datePickerVC.delegate = self
+            }
+        }
+    }
+    
+
+}
+
+extension TimeCircuitsViewController: DatePickerDelegate {
+    func destinationDateWasChosen(date: Date) {
+        destinationLabel.text = dateFormatter.string(from: date)
+    }
+    
+    
+}
---
+++ outatime/OutaTime/OutaTime/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  OutaTime
+//
+//  Created by Robert B on 9/19/19.
+//  Copyright © 2019 lambda school. All rights reserved.
+//
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
