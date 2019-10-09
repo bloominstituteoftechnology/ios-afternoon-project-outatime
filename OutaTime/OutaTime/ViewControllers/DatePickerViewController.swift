ce84a1c6d359876b714b7a4135bd02f527edcb4a
Gavin Murphy <gavincmurphy@gmail.com>
2019-10-08 19:46:10 -0700

    Created the UI for my TimeCircuitsViewController by adding buttons and labels and constraints

--- OutaTime/OutaTime/ViewControllers/DatePickerViewController.swift
+++ OutaTime/OutaTime/ViewControllers/DatePickerViewController.swift
@@ -2,8 +2,29 @@
 //  DatePickerViewController.swift
 //  OutaTime
 //
-//  Created by Gavin Murphy on 10/7/19.
+//  Created by Gavin Murphy on 10/8/19.
 //  Copyright © 2019 Gavin Murphy. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class DatePickerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
