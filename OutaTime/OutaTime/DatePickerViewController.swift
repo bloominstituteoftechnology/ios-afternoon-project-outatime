38a5e81e18a32c998a1e8cd5423d391f5bf12226
Dennis Rudolph <Dennis@Denniss-MacBook-Pro.local>
2019-10-16 18:32:53 -0400

    Reset

--- OutaTime/OutaTime/DatePickerViewController.swift
+++ OutaTime/OutaTime/DatePickerViewController.swift
@@ -8,27 +8,3 @@
 
 import UIKit
 
-protocol DatePickerDelegate {
-    func destinationDateWasChosen(date: Date)
-}
-
-class DatePickerViewController: UIViewController {
-    
-    @IBOutlet weak var datePicker: UIDatePicker!
-    
-    var delegate: DatePickerDelegate?
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-   
-    @IBAction func cancelButtonTapped(_ sender: Any) {
-        dismiss(animated: true, completion: nil)
-    }
-    
-    @IBAction func doneButtonTapped(_ sender: Any) {
-        delegate?.destinationDateWasChosen(date: datePicker.date)
-        dismiss(animated: true, completion: nil)
-    }
-
-}
