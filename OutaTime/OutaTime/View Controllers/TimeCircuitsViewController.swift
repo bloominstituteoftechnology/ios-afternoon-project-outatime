4a147c55824a96cbb508ef578399b1449d78c5e9
Jon Bash <jonbashmusic@gmail.com>
2019-10-16 13:52:53 -0700

    set tcvc as dpdelegate

--- OutaTime/OutaTime/View Controllers/TimeCircuitsViewController.swift
+++ OutaTime/OutaTime/View Controllers/TimeCircuitsViewController.swift
@@ -67,3 +67,9 @@ class TimeCircuitsViewController: UIViewController {
     */
 
 }
+
+extension TimeCircuitsViewController: DatePickerDelegate {
+    func destinationDateWasChosen(_ date: Date) {
+        //
+    }
+}
