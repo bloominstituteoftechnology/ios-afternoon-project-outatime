4eb67ba4326813e127ff1af56ba4a8c4614de880
Jon Bash <jonbashmusic@gmail.com>
2019-10-16 13:58:04 -0700

    set up segue prep

--- OutaTime/OutaTime/View Controllers/TimeCircuitsViewController.swift
+++ OutaTime/OutaTime/View Controllers/TimeCircuitsViewController.swift
@@ -56,16 +56,17 @@ class TimeCircuitsViewController: UIViewController {
     }
     
 
-    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "DestinationDatePickerSegue" {
+            guard let datePickerVC = segue.destination as? DatePickerViewController
+                else { return }
+            
+            datePickerVC.delegate = self
+        }
     }
-    */
-
 }
 
 extension TimeCircuitsViewController: DatePickerDelegate {
