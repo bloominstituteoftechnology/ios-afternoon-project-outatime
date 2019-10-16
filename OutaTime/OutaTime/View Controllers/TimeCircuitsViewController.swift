059633dd459fc8124ee99f45ea05b494ffdc2ac6
Jon Bash <jonbashmusic@gmail.com>
2019-10-16 13:22:46 -0700

    wire up timecircuits viewcontroller

--- OutaTime/OutaTime/View Controllers/TimeCircuitsViewController.swift
+++ OutaTime/OutaTime/View Controllers/TimeCircuitsViewController.swift
@@ -9,11 +9,27 @@
 import UIKit
 
 class TimeCircuitsViewController: UIViewController {
+    
+    // MARK: Properties
+    
+    // MARK: Labels
+    @IBOutlet weak var destinationTimeLabel: UILabel!
+    @IBOutlet weak var presentTimeLabel: UILabel!
+    @IBOutlet weak var lastDepartedTimeLabel: UILabel!
+    @IBOutlet weak var speedLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
+    
+    // MARK: UI Actions
+    
+    @IBAction func setDestinationTapped(_ sender: Any) {
+    }
+    
+    @IBAction func travelBackTapped(_ sender: Any) {
     }
     
 
