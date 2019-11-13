0e9fce750e75b30724dd80172289a84c6fda40d2
Chad Rutherford <chad_rutherford@outlook.com>
2019-11-13 17:01:21 -0500

    Added logic inside 'TimeCircuitsViewController' for initialization

--- OutaTime/OutaTime/Controller/TimeCircuitsViewController.swift
+++ OutaTime/OutaTime/Controller/TimeCircuitsViewController.swift
@@ -10,13 +10,37 @@ import UIKit
 
 class TimeCircuitsViewController: UIViewController {
     
+    // --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+    // MARK: - Outlets
     @IBOutlet weak var destinationTimeLabel: UILabel!
     @IBOutlet weak var presentTimeLabel: UILabel!
     @IBOutlet weak var lastTimeDepartedLabel: UILabel!
     @IBOutlet weak var speedLabel: UILabel!
     
+    // --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+    // MARK: - Properties
+    var dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .medium
+        return formatter
+    }()
+    
+    var currentSpeed = 0
+    var speedString: String {
+        return "\(currentSpeed) MPH"
+    }
+    
+    // --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+    // MARK: - View Controller Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateViews()
+    }
+    
+    func updateViews() {
+        presentTimeLabel.text = dateFormatter.string(from: Date())
+        lastTimeDepartedLabel.text = "--- -- ----"
+        speedLabel.text = speedString
     }
     
     @IBAction func travelBackTapped(_ sender: UIButton) {
