4e7e70f90ebcb66d400cf6e8c9fffd5f8c03afcf
Osha Washington <washingtonosha209@yahoo.com>
2019-12-10 22:06:35 -0800

    Storyborad wired and methods created

--- OutaTime/TimeCircuitsViewController.swift
+++ OutaTime/TimeCircuitsViewController.swift
@@ -9,10 +9,67 @@
 import UIKit
 
 class TimeCircuitsViewController: UIViewController {
-
+    
+    
+    // MARK: - IBOutlets
+    @IBOutlet weak var destinationTime: UILabel!
+    @IBOutlet weak var presentTime: UILabel!
+    @IBOutlet weak var lastTimeDeparted: UILabel!
+    @IBOutlet weak var speed: UILabel!
+    
+    // MARK: - IBActions
+    
+    @IBAction func setButtonTapped(_ sender: UIButton) {
+        speed.text = "0 MPH"
+    }
+    @IBAction func travelBackTapped(_ sender: UIButton) {
+        startTimer()
+    }
+    
+    // MARK: - Methods
+    func startTimer() {
+        if currentSpeed == 0 {
+            timer = Timer.scheduledTimer(withTimeInterval: 0.01, repeats: true, block: updateSpeed(timer:))
+        } else { currentSpeed = 0 }
+    }
+    func resetTimer() {
+        timer?.invalidate()
+        timer = nil
+    }
+     
+    func updateSpeed(timer: Timer) {
+        if currentSpeed < 88 {
+            currentSpeed += 1
+            speed.text = "\(currentSpeed) MPH"
+            lastTimeDeparted.text = destinationTime.text
+        }
+        if currentSpeed == 88 {
+            presentTime.text = destinationTime.text
+            resetTimer()
+        }
+    }
+    
+    func dateFormat() -> String {
+        let date = Date()
+        return dateFormatter.string(from: date)
+    }
+    
+    private var dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "MMM dd, yyyy"
+        formatter.timeZone = TimeZone(secondsFromGMT: 0)
+            return formatter
+        }()
+    
+    let currentDate = Date()
+    var currentSpeed = 0
+    var timer: Timer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        presentTime.text = dateFormat()
+        speed.text = String("\(currentSpeed) MPH")
+        lastTimeDeparted.text = "--- -- ----"
         // Do any additional setup after loading the view.
     }
     
