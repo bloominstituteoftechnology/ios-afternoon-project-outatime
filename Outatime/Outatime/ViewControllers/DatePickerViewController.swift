b4e8939a271eb035ae12d0cd5fe5961d0a373a03
Christopher Price <christopher.r.price@gmail.com>
2019-12-10 23:29:55 -0500

    Wired up the UIs. Finished the UI for the DatePickerViewController.

--- Outatime/Outatime/ViewControllers/DatePickerViewController.swift
+++ Outatime/Outatime/ViewControllers/DatePickerViewController.swift
@@ -10,21 +10,16 @@ import UIKit
 
 class DatePickerViewController: UIViewController {
 
+    @IBOutlet weak var datePicker: UIPickerView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func cancelTapped(_ sender: Any) {
     }
-    */
-
+    
+    @IBOutlet weak var doneTapped: NSLayoutConstraint!
+    
 }
--- Outatime/Outatime/ViewControllers/TimeCircuitsViewController.swift
+++ Outatime/Outatime/ViewControllers/TimeCircuitsViewController.swift
@@ -10,21 +10,17 @@ import UIKit
 
 class TimeCircuitsViewController: UIViewController {
 
+    @IBOutlet weak var destinationTime: UILabel!
+    @IBOutlet weak var presentTime: UILabel!
+    @IBOutlet weak var lastTimeDeparted: UILabel!
+    @IBOutlet weak var speed: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func travelBackTapped(_ sender: Any) {
     }
-    */
-
+    
 }
