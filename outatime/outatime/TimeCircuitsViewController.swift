1d39ec62bd244605e8f95caefc66fad0515ba972
DavidSchwinne <davidschwinne@gmail.com>
2019-12-12 11:31:49 +0900

    First  UI set up

---
+++ outatime/outatime/TimeCircuitsViewController.swift
@@ -0,0 +1,45 @@
+//
+//  TimeCircuitsViewController.swift
+//  outatime
+//
+//  Created by LaFleur on 12/12/19.
+//  Copyright © 2019 David Schwinne. All rights reserved.
+//
+
+import UIKit
+
+class TimeCircuitsViewController: UIViewController {
+
+    @IBOutlet weak var destinationTimeLabel: UILabel!
+    
+    @IBOutlet weak var presentTimeLabel: UILabel!
+    
+    @IBOutlet weak var lastTimeDepartedLabel: UILabel!
+    
+    @IBOutlet weak var speedLabel: UILabel!
+    
+    @IBAction func setDestinationTimeButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func travelBackButton(_ sender: UIButton) {
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
