2b338b15e887cd6ed2bd7a6625470aca59c07223
Jorge Alvarez <jorgelalvarez52@gmail.com>
2019-12-18 14:05:59 -0800

    finished CT1

--- OutaTime/OutaTime/TimeCircuitsViewController.swift
+++ OutaTime/OutaTime/TimeCircuitsViewController.swift
@@ -11,12 +11,17 @@ import UIKit
 class TimeCircuitsViewController: UIViewController {
 
     @IBOutlet weak var destinationTimeLabel: UILabel!
-    
     @IBOutlet weak var presentTimeLable: UILabel!
-    
     @IBOutlet weak var lastTimeDepartedLabel: UILabel!
-    
     @IBOutlet weak var speedLabel: UILabel!
+    
+    var dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "MMM d, yyyy" // JAN 01, 2001
+        return formatter
+    }()
+    
+    var currentSpeed = 0
     
     @IBAction func travelBackButtonTapped(_ sender: UIButton) {
         print("travel back")
@@ -24,8 +29,10 @@ class TimeCircuitsViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        presentTimeLable.text = dateFormatter.string(from: Date()) // current time
+        speedLabel.text = "\(currentSpeed) MPH"
+        lastTimeDepartedLabel.text = "--- -- ----"
+        
     }
     
 
