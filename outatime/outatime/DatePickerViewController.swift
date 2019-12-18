e9779cc18d5f8973c3ff5c9286c7f235ae4116b4
DavidSchwinne <davidschwinne@gmail.com>
2019-12-18 11:42:38 +0900

    Bugs need fixed

--- outatime/outatime/DatePickerViewController.swift
+++ outatime/outatime/DatePickerViewController.swift
@@ -8,23 +8,28 @@
 
 import UIKit
 
+protocol DatePickerDelegate {
+    
+    func destinationDateWasChosen(date: Date)
+}
+
 class DatePickerViewController: UIViewController {
 
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    var delegate: DatePickerDelegate?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func backButtonTapped(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
     }
-    */
-
+    
+    @IBAction func doneButtonTapped(_ sender: Any) {
+        delegate?.destinationDateWasChosen(date: datePicker.date)
+        dismiss(animated: true, completion: nil)
+    }
 }
--- outatime/outatime/TimeCircuitsViewController.swift
+++ outatime/outatime/TimeCircuitsViewController.swift
@@ -21,25 +21,80 @@ class TimeCircuitsViewController: UIViewController {
     @IBAction func setDestinationTimeButton(_ sender: UIButton) {
     }
     
-    @IBAction func travelBackButton(_ sender: UIButton) {
+    @IBAction func travelBackButtonTapped(_ sender: UIButton) {
+        if destinationTimeLabel.text != presentTimeLabel.text {
+            startTimer()
+                   } else {
+                       let alert = UIAlertController(title: "Time Travel Failed", message: "You are already in \(destinationTimeLabel.text!)", preferredStyle: .alert)
+                       alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                       present(alert, animated: true, completion: nil)
+                   }
     }
     
+    
+    var dateFormatter: DateFormatter = {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "HH:mm:ss:SS"
+        dateFormatter.timeZone = TimeZone.init(secondsFromGMT: 0)
+        return dateFormatter
+    }()
+    
+    var speed = 0
+    
+    var timer: Timer?
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "ShowDestinationTimeSegue" {
+            if let datePickerVC = segue.destination as? DatePickerViewController {
+                datePickerVC.delegate = self as! DatePickerDelegate
+            }
+        }
     }
-    */
+            
+        
+    func startTimer() {
+        timer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true, block: updateSpeed(time:))
+        }
+        
+    private func resetTimer() {
+        timer?.invalidate()
+        timer = nil
+        }
+        
+    func updateSpeed(time: Timer) {
+        if speed >= 88 {
+            resetTimer()
+            
+            presentTimeLabel.text = destinationTimeLabel.text
+            
+            speed = 0
+            
+            lastTimeDepartedLabel.text = presentTimeLabel.text
+            
+            speedLabel.text = "\(speed)MPH"
+            showAlert()
+        } else {
+            speed += 1
+            speedLabel.text = "\(speed)MPH"
+        }
+    }
+        
+     func showAlert() {
+        let alert = UIAlertController(title: "Time Travel Successful!", message: "Your new date is \(presentTimeLabel.text!)", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK!", style: .default, handler: nil))
+            present(alert, animated: true, completion: nil)
+    }
+        
 
+    extension TimeCircuitsViewController: DatePickerDelegate {
+        func destinationDateWasChosen(date: Date) {
+            destinationTimeLabel.text = dateFormatter.string(from: date)
+        }
+    }
 }
