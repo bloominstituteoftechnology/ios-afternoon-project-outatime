6a664c6c5370ab7ae3b267bafd5d50e661ad84a0
Malik <malik21barnes@gmail.com>
2019-12-18 23:57:13 -0800

    ?

---
+++ OutaTime/OutaTime/View Controllers/DatePickerViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DatePickerViewController.swift
+//  OutaTime
+//
+//  Created by Malik Barnes on 12/18/19.
+//  Copyright © 2019 Malik Barnes. All rights reserved.
+//
+
+import UIKit
+
+class DatePickerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    @IBAction func cancelTapped(_ sender: UIBarButtonItem) {
+        
+    }
+    
+    
+    @IBAction func doneTapped(_ sender: UIBarButtonItem) {
+        
+    }
+}
---
+++ OutaTime/OutaTime/View Controllers/TimeCircuitsViewController.swift
@@ -0,0 +1,51 @@
+//
+//  TimeCircuitsViewController.swift
+//  OutaTime
+//
+//  Created by Malik Barnes on 12/18/19.
+//  Copyright © 2019 Malik Barnes. All rights reserved.
+//
+
+import UIKit
+
+class TimeCircuitsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    // MARK: - Outlets
+    
+    @IBOutlet weak var destinationTime: UILabel!
+    @IBOutlet weak var presentTime: UILabel!
+    @IBOutlet weak var lastTimeDeparted: UILabel!
+    @IBOutlet weak var speed: UILabel!
+    @IBOutlet weak var setDestinationButton: UIButton!
+    @IBOutlet weak var travelBackButton: UIButton!
+    
+    // MARK: - Actions
+    
+    @IBAction func travelBackButtonTapped(_ sender: UIButton) {
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
