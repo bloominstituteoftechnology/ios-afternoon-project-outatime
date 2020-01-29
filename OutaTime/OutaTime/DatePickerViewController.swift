1d0b16d8c11e3efa9cd256c72b3df6f5b13864d4
Chris Gonzales <57194564+supergonzo812@users.noreply.github.com>
2020-01-29 15:14:08 -0800

    Finished project

--- OutaTime/OutaTime/DatePickerViewController.swift
+++ OutaTime/OutaTime/DatePickerViewController.swift
@@ -8,32 +8,38 @@
 
 import UIKit
 
+protocol DatePickerDelegate{
+    func destinationDateWasChosen(date: Date)
+}
+
 class DatePickerViewController: UIViewController {
 
+    var delegate: DatePickerDelegate?
+       
+    
     @IBOutlet weak var datePicker: UIDatePicker!
     
 
     @IBAction func cancelTapped(_ sender: Any) {
+        
+        dismiss(animated: true, completion: nil)
     }
     
     @IBAction func doneTapped(_ sender: UIButton) {
+       
+        delegate?.destinationDateWasChosen(date: datePicker.date)
+        dismiss(animated: true, completion: nil)
+        
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
+    
+    
 
 }
--- OutaTime/OutaTime/TimeCurcuitViewController.swift
+++ OutaTime/OutaTime/TimeCurcuitViewController.swift
@@ -9,37 +9,85 @@
 import UIKit
 
 class TimeCurcuitViewController: UIViewController {
-
+    
+    var speed: Int = 0
+    var timer: Timer? = nil
+    
+    var dateFormatter: DateFormatter{
+        let formatter = DateFormatter()
+        formatter.dateFormat = "MMM d, yyyy"
+        formatter.timeZone = TimeZone(secondsFromGMT: 0)
+        return formatter
+    }
     
     @IBOutlet weak var destinationDateLabel: UILabel!
-    
     @IBOutlet weak var presentDateLabel: UILabel!
-    
     @IBOutlet weak var lastDateLabel: UILabel!
-    
     @IBOutlet weak var mPHLabel: UILabel!
     
-    
-    
-    
     @IBAction func travelBackTapped(_ sender: UIButton) {
+        timer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true, block: updateSpeed(timer:)
+        )}
+    func resetTimer(){
+        timer?.invalidate()
+        timer = nil
     }
+    
+    func updateSpeed(timer: Timer){
+        if speed < 88{
+            speed += 1
+            mPHLabel.text = "\(speed) MPH"
+        }else {
+            resetTimer()
+            lastDateLabel.text = presentDateLabel.text
+            presentDateLabel.text = destinationDateLabel.text
+            speed = 0
+            mPHLabel.text = "0 MPH"
+            showAlert()
+       
+        }
+    }
+    
+    func showAlert(){
+        let alert = UIAlertController(title: "Time Travel Successful",message: "You're new date is \( presentDateLabel.text!)", preferredStyle: .alert)
+                   alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+                   present(alert, animated: true,
+                           completion: nil)
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        presentDateLabel.text = dateFormatter.string(from: Date())
+        mPHLabel.text = "\(speed) MPH"
+        lastDateLabel.text = " --- -- ----"
+        
     }
     
-
-    /*
     // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "ModalDestinationDatePickerSegue"{
+            if let datePickerVC = segue.destination as? DatePickerViewController{
+                datePickerVC.delegate = self
+//                if let destLabel = destinationDateLabel.text{
+//                    destLabel = dateFormatter.string(from: datePickerVC.datePicker.date)
+//                }
+            }
+        }
+        
+        
     }
-    */
+    
+    
+}
 
+
+extension TimeCurcuitViewController: DatePickerDelegate{
+    func destinationDateWasChosen(date: Date) {
+        destinationDateLabel.text = dateFormatter.string(from: date)
+    }
+    
+    
 }
