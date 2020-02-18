21f9cd20dde4d5a4f227379ad907f896eda6cbdd
Jarren Campos <jarrenmcampos@gmail.com>
2020-02-17 18:24:17 -0800

    Initial Setup

---
+++ OutaTime/OutaTime/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OutaTime
+//
+//  Created by Jarren Campos on 2/17/20.
+//  Copyright © 2020 Jarren Campos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
