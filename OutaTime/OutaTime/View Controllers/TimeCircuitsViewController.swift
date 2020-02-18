1d8dd384dacb08c09f53334cd2ddd95163a22c7c
Breanna Young <bree.j.32@gmail.com>
2020-02-18 00:56:56 -0500

    added IBOutlets to timeCircuitsViewController

--- OutaTime/OutaTime/View Controllers/TimeCircuitsViewController.swift
+++ OutaTime/OutaTime/View Controllers/TimeCircuitsViewController.swift
@@ -8,6 +8,30 @@
 
 import Foundation
 
+
 class TimeCircuitsViewController {
     
+    // MARK: Outlets
+    
+    @IBOutlet weak var destinationTimeDatePicker: UIDatePicker!
+    @IBOutlet weak var presentTimeDatePicker: UIDatePicker!
+    @IBOutlet weak var lastTimeDepartedDatePicker: UIDatePicker!
+    @IBOutlet weak var speedDatePicker: UIPickerView!
+    @IBOutlet weak var setDestinationTime: UIButton!
+    @IBOutlet weak var travelBack: UIButton!
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
 }
