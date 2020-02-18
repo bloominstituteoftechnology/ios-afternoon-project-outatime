834b8251d65481d674cf759d12d3ab224c369d17
Christopher DeVito <toolofjesus@protonmail.com>
2020-02-18 01:10:34 -0500

    Set up storyboard

---
+++ OutaTime/OutaTime/ViewControllers/DatePickerViewController.swift
@@ -0,0 +1,45 @@
+//
+//  DatePickerViewController.swift
+//  OutaTime
+//
+//  Created by Lambda_School_Loaner_259 on 2/18/20.
+//  Copyright © 2020 DeVitoC. All rights reserved.
+//
+
+import UIKit
+
+class DatePickerViewController: UIViewController {
+    
+    // MARK: IBOutlets
+    @IBOutlet var datePickerView: UIDatePicker!
+    
+    
+    //MARK: IBActions
+    @IBAction func doneTapped(_ sender: Any) {
+        
+    }
+    
+    @IBAction func cancelTapped(_ sender: Any) {
+        
+    }
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ OutaTime/OutaTime/ViewControllers/TimeCircuitsViewController.swift
@@ -0,0 +1,49 @@
+//
+//  TimeCircuitsViewController.swift
+//  OutaTime
+//
+//  Created by Lambda_School_Loaner_259 on 2/18/20.
+//  Copyright © 2020 DeVitoC. All rights reserved.
+//
+
+import UIKit
+
+class TimeCircuitsViewController: UIViewController {
+    
+    // MARK: IBOutlets
+    @IBOutlet var destinationLabel: UILabel!
+    @IBOutlet var presentLabel: UILabel!
+    @IBOutlet var lastTimeLabel: UILabel!
+    @IBOutlet var speedLabel: UILabel!
+    
+    // MARK: Properties
+    
+    
+    // MARK: IBActions
+    @IBAction func setDestinationTapped(_ sender: Any) {
+    }
+    
+    @IBAction func travelBackTapped(_ sender: Any) {
+    }
+    
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
