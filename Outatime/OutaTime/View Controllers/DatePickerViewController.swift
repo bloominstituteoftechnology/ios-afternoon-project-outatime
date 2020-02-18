e07b3e69cd3f516bc56fd4341e5e5f6696e95fbd
Libby Thomas <thomaslibby05@gmail.com>
2020-02-18 11:30:44 -0600

    Storyboards complete

--- Outatime/OutaTime/View Controllers/DatePickerViewController.swift
+++ Outatime/OutaTime/View Controllers/DatePickerViewController.swift
@@ -9,22 +9,28 @@
 import UIKit
 
 class DatePickerViewController: UIViewController {
+    
+    // MARK: - IBOutlets
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    // MARK: - Properties
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    
+    // MARK: - IBActions
+    @IBAction func cancelButtonTapped(_ sender: UIBarButtonItem) {
+        
+    }
+    
+    @IBAction func doneButtonTapped(_ sender: UIBarButtonItem) {
+        
     }
     
 
-    /*
-    // MARK: - Navigation
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        datePicker.datePickerMode = .date
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
     }
-    */
-
 }
--- Outatime/OutaTime/View Controllers/TimeCircuitsViewController.swift
+++ Outatime/OutaTime/View Controllers/TimeCircuitsViewController.swift
@@ -9,22 +9,37 @@
 import UIKit
 
 class TimeCircuitsViewController: UIViewController {
-
+    
+    // MARK: - IBOutlets
+    @IBOutlet weak var destinationTimeLabel: UILabel!
+    @IBOutlet weak var presentTimeLabel: UILabel!
+    @IBOutlet weak var lastTimeDepartedLabel: UILabel!
+    @IBOutlet weak var speedLabel: UILabel!
+    
+    // MARK: - Properties
+    
+    var dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "MMM dd, yyyy"
+        formatter.timeZone = TimeZone(secondsFromGMT: 0)
+        return formatter
+    }()
+    
+    
+    
+    
+    
+    
+    // MARK: - IBActions
+    @IBAction func travelBackButtonTapped(_ sender: UIButton) {
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
