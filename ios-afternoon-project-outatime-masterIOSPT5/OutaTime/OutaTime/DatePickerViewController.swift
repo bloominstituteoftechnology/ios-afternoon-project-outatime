ca381527d7a437c58c1f24924fc67336c46a921d
David Williams <burmesedragon@att.net>
2020-02-18 16:08:37 -0800

    app works. Constraints need attention

--- ios-afternoon-project-outatime-masterIOSPT5/OutaTime/OutaTime/DatePickerViewController.swift
+++ ios-afternoon-project-outatime-masterIOSPT5/OutaTime/OutaTime/DatePickerViewController.swift
@@ -8,23 +8,31 @@
 
 import UIKit
 
+protocol DatePickerDelegate {
+    func destinationDateWasChosen(date: Date)
+}
+
 class DatePickerViewController: UIViewController {
 
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .darkGray
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    var delegate: DatePickerDelegate?
+  
+    
+    
+    @IBAction func cancelButtonTapped(_ sender: Any) {
+        dismiss(animated: true)
     }
-    */
-
+    
+    @IBAction func doneButtonTapped(_ sender: Any) {
+        if let date = datePicker?.date {
+            self.delegate?.destinationDateWasChosen(date: date)
+        }
+        dismiss(animated: true)
+    }
 }
