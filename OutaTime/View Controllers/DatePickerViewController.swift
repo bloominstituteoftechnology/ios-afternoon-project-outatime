a58b9ac7851d8d1a578f140840260de3228eec72
Chad Parker <chad@chadparker.com>
2020-02-19 12:40:42 -0800

    Storyboard setup done

---
+++ OutaTime/View Controllers/DatePickerViewController.swift
@@ -0,0 +1,26 @@
+//
+//  DatePickerViewController.swift
+//  OutaTime
+//
+//  Created by Chad Parker on 2/18/20.
+//  Copyright © 2020 Chad Parker. All rights reserved.
+//
+
+import UIKit
+
+class DatePickerViewController: UIViewController {
+
+    @IBOutlet weak var datePicker: UIDatePicker!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func cancelButtonTapped(_ sender: UIButton) {
+
+    }
+
+    @IBAction func doneButtonTapped(_ sender: UIButton) {
+
+    }
+}
---
+++ OutaTime/View Controllers/TimeCircuitsViewController.swift
@@ -0,0 +1,32 @@
+//
+//  TimeCircuitsViewController.swift
+//  OutaTime
+//
+//  Created by Chad Parker on 2/18/20.
+//  Copyright © 2020 Chad Parker. All rights reserved.
+//
+
+import UIKit
+
+class TimeCircuitsViewController: UIViewController {
+
+    @IBOutlet weak var destinationTimeLabel: UILabel!
+    @IBOutlet weak var presentTimeLabel: UILabel!
+    @IBOutlet weak var lastTimeDepartedLabel: UILabel!
+    @IBOutlet weak var speedLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func travelBackButtonPressed(_ sender: UIButton) {
+
+    }
+
+    /*
+    // MARK: - Navigation
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    }
+    */
+}
