ba3bef2d842fec91eaf4c8cdc0b876960199a414
Jarren Campos <jarrenmcampos@gmail.com>
2020-02-18 20:14:11 -0800

    Added present day time/date functionality

--- OutaTime/OutaTime/View Controllers/DatePickerViewController.swift
+++ OutaTime/OutaTime/View Controllers/DatePickerViewController.swift
@@ -8,23 +8,30 @@
 
 import UIKit
 
-class DatePickerViewController: UIViewController {
+protocol DatePickerDelegate {
+    func destinationDateWasChosen(_: Date)
+}
 
+class DatePickerViewController: UIViewController {
+    
+    //MARK: -IBOutlets
+    @IBOutlet var datePicker: UIDatePicker!
+    
+    var delegate: DatePickerDelegate?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
-    
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    //MARK: - IBActions
+    @IBAction func cancelButton(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
     }
-    */
+    @IBAction func doneButton(_ sender: UIButton) {
+        
+    }
+    
 
 }
--- OutaTime/OutaTime/View Controllers/TimeCircuitsViewController.swift
+++ OutaTime/OutaTime/View Controllers/TimeCircuitsViewController.swift
@@ -9,22 +9,82 @@
 import UIKit
 
 class TimeCircuitsViewController: UIViewController {
-
+    
+    //MARK: -IBOutlets
+    
+    //Destination
+    @IBOutlet var desMonthLabel: UILabel!
+    @IBOutlet var desDayLabel: UILabel!
+    @IBOutlet var desYearLabel: UILabel!
+    @IBOutlet var desTimeLabel: UILabel!
+    //Present
+    @IBOutlet var presMonthLabel: UILabel!
+    @IBOutlet var presDayLabel: UILabel!
+    @IBOutlet var presYearLabel: UILabel!
+    @IBOutlet var presTimeLabel: UILabel!
+    //Last Time Departed
+    @IBOutlet var lasMonthLabel: UILabel!
+    @IBOutlet var lasDayLabel: UILabel!
+    @IBOutlet var lasYearLabel: UILabel!
+    @IBOutlet var lasTimeLabel: UILabel!
+    //MPH
+    @IBOutlet var mphLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        updateViews()
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    //MARK: - IBActions
+    @IBAction func travelBackButton(_ sender: Any) {
+        
     }
-    */
+    var dateformatter: DateFormatter = {
+        let formatter = DateFormatter()
+        //universal by looking at formats
+        formatter.dateFormat = "HH:mm"
+        formatter.timeZone = TimeZone.current
+        return formatter
+    }()
+    
+    func updateViews() {
+        let date = Date()
+        presMonthLabel.text = date.monthAsString()
+        presDayLabel.text = date.dayAsString()
+        presYearLabel.text = date.yearAsString()
+        presTimeLabel.text = dateformatter.string(from: date)
+    }
+}
+//getting the current date/time
+extension Date {
+    func monthAsString() -> String {
+        let df = DateFormatter()
+        df.setLocalizedDateFormatFromTemplate("MMM")
+        return df.string(from: self)
+    }
+    func dayAsString() -> String {
+        let df = DateFormatter()
+        df.setLocalizedDateFormatFromTemplate("dd")
+        return df.string(from: self)
+    }
+    func yearAsString() -> String {
+        let df = DateFormatter()
+        df.setLocalizedDateFormatFromTemplate("YYYY")
+        return df.string(from: self)
+    }
+    func timeAsString() -> String {
+        let df = DateFormatter()
+        df.setLocalizedDateFormatFromTemplate("HH:mm")
+        return df.string(from: self)
+    }
+}
 
+extension TimeCircuitsViewController: DatePickerDelegate{
+    func destinationDateWasChosen(_: Date) {
+        <#code#>
+    }
+    
+    
 }
