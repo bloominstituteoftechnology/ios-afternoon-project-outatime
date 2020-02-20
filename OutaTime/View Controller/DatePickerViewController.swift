4fe29ba9df8e00caed42a80520b271bda65c7625
Robert Vance <Fearmydotzs@gmail.com>
2020-02-19 18:36:37 -0700

    Completed OutaTime Project

--- OutaTime/View Controller/DatePickerViewController.swift
+++ OutaTime/View Controller/DatePickerViewController.swift
@@ -12,20 +12,15 @@ protocol DatePickerDelegate {
     func destinationDateWasChosen(date: Date)
 }
 
-
 class DatePickerViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
     var delegate: DatePickerDelegate?
 
-   
     @IBOutlet weak var datePicker: UIDatePicker!
     
-    
-    
-  
     @IBAction func doneTapped(_ sender: Any) {
         delegate?.destinationDateWasChosen(date: datePicker.date)
         dismiss(animated: true, completion: nil)
@@ -34,5 +29,4 @@ class DatePickerViewController: UIViewController {
     @IBAction func cancelTapped(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
-    
 }
--- OutaTime/View Controller/TimeCircuitsViewController.swift
+++ OutaTime/View Controller/TimeCircuitsViewController.swift
@@ -16,32 +16,72 @@ class TimeCircuitsViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
+        updateViews()
     }
-   
-    @IBAction func travelBackPressed(_ sender: UIButton) {
+    
+    func updateViews() {
+           presentTimeLabel.text = dateFormatter.string(from: Date())
+           speedLabel.text = "\(speed) MPH"
+           lastTimeDepartedLabel.text = "--- -- ----"
+           destinationTimeLabel.text = "Jan 01 2000"
+       }
+    
+       @IBAction func travelBackPressed(_ sender: UIButton) {
+           startTimer()
+       }
+    
+       var speed = 0
+       var timer: Timer?
        
-    }
-    var speed = 0
-    var timer: Timer?
+       private var dateFormatter: DateFormatter {
+           let formatter = DateFormatter()
+           formatter.dateFormat = "MMM d, yyyy"
+           formatter.timeZone = TimeZone(secondsFromGMT: 0)
+           return formatter
+       }
     
-    private var dateFormatter: DateFormatter {
-        let formatter = DateFormatter()
-        formatter.dateFormat = "MMM d, yyyy"
-        formatter.timeZone = TimeZone(secondsFromGMT: 0)
-        return formatter
-    }
+       func startTimer() {
+           timer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true, block: updateSpeed(timer:))
+       }
+       
+       func resetTimer() {
+           timer?.invalidate()
+           timer = nil
+       }
     
-    func showAlert() {
-        let alert = UIAlertController(title: "Time Travel Successful!", message: "Your new date: \(presentTimeLabel.text ?? "Error")", preferredStyle: .alert)
-        let action = UIAlertAction(title: "Okay", style: .default, handler: nil)
-        alert.addAction(action)
-        present(alert, animated: true)
-    }
+       func updateSpeed(timer: Timer) {
+           if speed >= 88 {
+               resetTimer()
+               speedLabel.text = "0 MPH"
+               lastTimeDepartedLabel.text = presentTimeLabel.text
+               presentTimeLabel.text = destinationTimeLabel.text
+               speed = 0
+               showAlert()
+           } else {
+               speed += 1
+               speedLabel.text = "\(speed) MPH"
+           }
+       }
     
-   
+       func showAlert() {
+           let alert = UIAlertController(title: "Time Travel Successful!", message: "Your new date: \(presentTimeLabel.text ?? "Error")", preferredStyle: .alert)
+           let action = UIAlertAction(title: "Ok", style: .default, handler: nil)
+           alert.addAction(action)
+           present(alert, animated: true)
+       }
+       
+       override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+           if segue.identifier == "ModalDestinationDatePickerSegue" {
+               guard let datePickerVC = segue.destination as? DatePickerViewController else { return }
+               datePickerVC.delegate = self
+           }
+       }
 
-    
-    
+   }
+
+   extension TimeCircuitsViewController: DatePickerDelegate {
+       func destinationDateWasChosen(date: Date) {
+           destinationTimeLabel.text = dateFormatter.string(from: date)
+           
+       }
 }
