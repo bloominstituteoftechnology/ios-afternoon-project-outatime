276415ea7993a6ed461bb797f09711adb63db981
Hector L <hectoremil21@hotmail.com>
2020-02-25 22:13:10 -0500

    Basic UI

---
+++ OutaTime/OutaTime/Controllers/TimeCircuitsViewController.swift
@@ -0,0 +1,40 @@
+//
+//  TimeCircuitsViewController.swift
+//  OutaTime
+//
+//  Created by Karen Rodriguez on 2/25/20.
+//  Copyright © 2020 Hector Ledesma. All rights reserved.
+//
+
+import UIKit
+
+class TimeCircuitsViewController: UIViewController {
+
+    @IBOutlet weak var destTimeLabel: UILabel!
+    @IBOutlet weak var presentTimeLabel: UILabel!
+    @IBOutlet weak var lastDepartLabel: UILabel!
+    @IBOutlet weak var speedLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func travelBack(_ sender: Any) {
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
