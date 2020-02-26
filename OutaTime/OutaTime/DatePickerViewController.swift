655297c224ca083fe67823b6a7b0613c4a81425d
Brad <braddiroff@icloud.com>
2020-02-26 18:38:09 -0500

    Finished project

--- OutaTime/OutaTime/DatePickerViewController.swift
+++ OutaTime/OutaTime/DatePickerViewController.swift
@@ -21,26 +21,76 @@ class DatePickerViewController: UIViewController {
     }
     
     @IBAction func doneTapped(_ sender: Any) {
-        
+        print(travelTime)
+        guard let dateItem = dateFormatter.date(from: travelTime) else {
+            return}
+        delegate?.destinationDateWasChosen(dateItem)
+        dismiss(animated: true, completion: nil)
     }
     
     var delegate: DatePickerDelegate?
     
+    let months: [String] = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+    
+    let days: [String] = Array(1...31).map { String($0) }
+    let years: [String] = Array(1885...2045).map { String($0) }
+    
+    var travelTime: String {
+        let month = datePicker.selectedRow(inComponent: 0)
+        let day = datePicker.selectedRow(inComponent: 1)
+        let year = datePicker.selectedRow(inComponent: 2)
+        
+        let stringDate = "\(months[month]) \(days[day]) \(years[year])"
+        print(stringDate)
+        return stringDate
+    }
+    
+    lazy private var dayPickerData: [[String]] = {
+        
+        let data: [[String]] = [months, days, years]
+        return data
+    }()
+    
+    let dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "MMM d yyyy"
+        formatter.timeZone = TimeZone(secondsFromGMT: 0)
+        return formatter
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        datePicker.delegate = self
+        datePicker.dataSource = self
+        
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
+
+extension DatePickerViewController: UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+
+        return dayPickerData.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+
+        return dayPickerData[component].count
+    }
+}
+
+extension DatePickerViewController: UIPickerViewDelegate {
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        let componentArray = dayPickerData[component]
+        let title = componentArray[row]
+        
+        return title
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        
+    }
+}
+
--- OutaTime/OutaTime/TimeCircuitsViewController.swift
+++ OutaTime/OutaTime/TimeCircuitsViewController.swift
@@ -15,8 +15,10 @@ class TimeCircuitsViewController: UIViewController {
     @IBOutlet var departedText: UILabel!
     @IBOutlet var speedText: UILabel!
     
+    @IBOutlet var travelBackButton: UIButton!
     
     @IBAction func travelBackTapped(_ sender: UIButton) {
+        startTimer()
     }
     
     let dateFormatter: DateFormatter = {
@@ -27,6 +29,7 @@ class TimeCircuitsViewController: UIViewController {
      }()
     
     var currentSpeed = 0
+    var myTimer: Timer?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,28 +37,64 @@ class TimeCircuitsViewController: UIViewController {
         let timeNow = Date()
         presentText?.text = dateFormatter.string(from: timeNow)
         speedText?.text = "\(currentSpeed) MPH"
+        destinationText?.text = "--- -- ----"
         departedText?.text = "--- -- ----"
         
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
+    func startTimer() {
+        myTimer = Timer(timeInterval: 0.1, repeats: true, block: updateSpeed(timer:))
+        if let timer = myTimer {
+            RunLoop.current.add(timer, forMode: .default)
+        }
+    }
+    
+    func updateSpeed(timer: Timer) {
+        if currentSpeed < 88 {
+            currentSpeed += 1
+            speedText?.text = "\(currentSpeed) MPH"
+        } else {
+            resetTimer()
+            departedText?.text = presentText?.text
+            presentText?.text = destinationText?.text
+            currentSpeed = 0
+            showAlert()
+        }
+    }
+    
+    private func showAlert() {
+        
+        guard let lastText = presentText.text else {return}
+        
+        let alert = UIAlertController(title: "Time Travel Successful", message: "Your new date is \(lastText)", preferredStyle: .alert)
+        
+        let okAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+        
+        alert.addAction(okAction)
+        
+        present(alert, animated: true, completion: nil)
+        
+    }
+    
+    func resetTimer() {
+        myTimer?.invalidate()
+        myTimer = nil
+    }
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        
+        if segue.identifier == "ModalDestinationDatePickerSegue" {
+        guard let datePickVC = segue.destination as? DatePickerViewController else {return}
+                datePickVC.delegate = self
+            }
     }
-    */
-
 }
 
 extension TimeCircuitsViewController: DatePickerDelegate {
     
     func destinationDateWasChosen(_ date: Date) {
-        <#code#>
+        destinationText?.text = dateFormatter.string(from: date)
     }
-    
 }
