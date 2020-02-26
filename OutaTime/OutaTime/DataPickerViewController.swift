fa04e60983ee010f53a6446225f24807a2360216
Samuel Esserman <samessermanlambda@gmail.com>
2020-02-26 16:13:27 -0500

    DatePickerVC - IBAction complete

--- OutaTime/OutaTime/DataPickerViewController.swift
+++ OutaTime/OutaTime/DataPickerViewController.swift
@@ -20,22 +20,16 @@ class DataPickerViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+      
     }
     
     @IBAction func cancelTapped(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
     }
     
     @IBAction func doneTapped(_ sender: Any) {
+        let date = datePickerView.date
+        delegate?.destinationDateWasChosen(date: date)
+        dismiss(animated: true, completion: nil)
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
