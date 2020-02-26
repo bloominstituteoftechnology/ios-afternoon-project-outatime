3f7140c682904267435923551abbaa7c3b61ac83
Nichole Davidson <nicholedavid@gmail.com>
2020-02-26 15:03:16 -0700

    Added 2 view controllers and formatted the tiem circut vc

--- OutaTime/OutaTime/Resources/SceneDelegate.swift
+++ OutaTime/OutaTime/Resources/SceneDelegate.swift
@@ -2,7 +2,7 @@
 //  SceneDelegate.swift
 //  OutaTime
 //
-//  Created by Nichole Davidson on 1/29/20.
+//  Created by Nichole Davidson on 2/26/20.
 //  Copyright © 2020 Nichole Davidson. All rights reserved.
 //
 
