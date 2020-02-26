bab9089d2a032f30b6f898143ebf5233d200386b
Samuel Esserman <samessermanlambda@gmail.com>
2020-02-26 15:34:34 -0500

    Almost done with storyboard section

--- OutaTime/OutaTime/TimeCircuitsViewController.swift
+++ OutaTime/OutaTime/TimeCircuitsViewController.swift
@@ -9,11 +9,19 @@
 import UIKit
 
 class TimeCircuitsViewController: UIViewController {
-
+    @IBOutlet weak var destinationTimeLabel: UILabel!
+    @IBOutlet weak var presentTimeLabel: UILabel!
+    @IBOutlet weak var lastTimeDepartedLabel: UILabel!
+    @IBOutlet weak var speedLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
+    
+    
+    
+    @IBAction func travelBackTapped(_ sender: Any) {
     }
     
 
