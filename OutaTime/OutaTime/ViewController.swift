95df8070f68194f422fa594bde7fe8cb93cef25c
Shawn-James <shawn.james@me.com>
2020-02-26 17:26:01 -0500

    Project created

---
+++ OutaTime/OutaTime/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OutaTime
+//
+//  Created by Shawn James on 2/26/20.
+//  Copyright © 2020 Shawn James. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
