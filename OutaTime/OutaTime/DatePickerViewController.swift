dc5a3a8b91becdf2528027272c8011c6d8571ae8
Joe VeVerka <joeveverka89@gmail.com>
2020-03-25 15:37:16 -0400

    created first UI

---
+++ OutaTime/OutaTime/DatePickerViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DatePickerViewController.swift
+//  OutaTime
+//
+//  Created by Marissa Gonzales on 3/25/20.
+//  Copyright © 2020 Joe Veverka. All rights reserved.
+//
+
+import UIKit
+
+class DatePickerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ OutaTime/OutaTime/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OutaTime
+//
+//  Created by Marissa Gonzales on 3/25/20.
+//  Copyright © 2020 Joe Veverka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
