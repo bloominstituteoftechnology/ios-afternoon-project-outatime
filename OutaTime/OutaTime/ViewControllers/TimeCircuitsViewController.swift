c5e7501df0a61c4616610622c739daf6bd83cc90
Enzo Jimenez-Soto <enzojimenezsoto@gmail>
2020-04-22 14:06:37 -0700

    Finsihed Storyboard

--- OutaTime/OutaTime/ViewControllers/TimeCircuitsViewController.swift
+++ OutaTime/OutaTime/ViewControllers/TimeCircuitsViewController.swift
@@ -9,22 +9,17 @@
 import UIKit
 
 class TimeCircuitsViewController: UIViewController {
-
+    
+    @IBOutlet weak var destinationTimeLabel: UILabel!
+    
+    @IBOutlet weak var presentTimeLabel: UILabel!
+    
+    @IBOutlet weak var lastTimeDepartedLabel: UILabel!
+    
+    @IBOutlet weak var speedLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+   
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
