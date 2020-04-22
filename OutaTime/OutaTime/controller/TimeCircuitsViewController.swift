93a21602d5b21f688e1adcc268cbba3fc7fa4b16
Vincent Hoang <vietnhoang7@gmail.com>
2020-04-22 14:04:15 -0700

    Finish implementing time travel functionality

--- OutaTime/OutaTime/controller/TimeCircuitsViewController.swift
+++ OutaTime/OutaTime/controller/TimeCircuitsViewController.swift
@@ -38,13 +38,60 @@ class TimeCircuitsViewController: UIViewController, DatePickerViewDelegate {
         os_log("Destination time set")
         destinationTimeLabel.text = dateFormatter.string(from: date)
     }
-
-    @IBAction func setDestinationButtonPressed(_ sender: UIButton) {
-        
-    }
     
     @IBAction func travelBackButtonPressed(_ sender: UIButton) {
+        startTimer()
         
+        
+        let alert = UIAlertController(title: "Time Travel Successful", message: "Your new date is \(presentTimeLabel.text ?? "--- -- ----")", preferredStyle: .alert)
+        
+        let dismissAction = UIAlertAction(title: "Done", style: .cancel,
+                                          handler: { action in self.resetTimer() })
+        
+    
+        
+        alert.addAction(dismissAction)
+        
+        present(alert, animated: true,
+                completion: { self.updateTime() })
+    }
+    
+    private func startTimer() {
+        var currentSpeed = 0
+
+        let timer = Timer.scheduledTimer(withTimeInterval: 0.01, repeats: true) { timer in
+            self.speedLabel.text = "\(currentSpeed) MPH"
+            
+            if currentSpeed == 88 {
+                timer.invalidate()
+            }
+            
+            currentSpeed += 1
+        }
+        
+        timer.fire()
+    }
+    
+    private func updateTime() {
+        lastDepartedLabel.text = presentTimeLabel.text
+        presentTimeLabel.text = destinationTimeLabel.text
+    }
+    
+    private func resetTimer() {
+        
+        var currentSpeed: Int = 88
+        
+        let timer = Timer.scheduledTimer(withTimeInterval: 0.01, repeats: true) { timer in
+            self.speedLabel.text = "\(currentSpeed) MPH"
+            
+            if currentSpeed == 0 {
+                timer.invalidate()
+            }
+            
+            currentSpeed -= 1
+        }
+        
+        timer.fire()
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
