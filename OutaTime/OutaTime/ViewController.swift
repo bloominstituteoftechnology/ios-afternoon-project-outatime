527044659988a3780c41b34447aa89d449d19032
Your Name Here <youremailhere>
2020-04-27 23:56:36 -0400

    Set up initial views and VC

---
+++ OutaTime/OutaTime/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  OutaTime
+//
+//  Created by Sammy Alvarado on 4/27/20.
+//  Copyright © 2020 Sammy Alvarado. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+
+
+}
+
