4ef896b211eea75b2a6458f4008db5a0af1fdb4a
violet-love <violetlove9419@icloud.com>
2020-05-20 15:46:28 -0700

    created outlets for the speed, last time departed, present time, ad destination time labels

--- OuttaTime/OuttaTime/ViewControllers/TimeCircuitsViewController.swift
+++ OuttaTime/OuttaTime/ViewControllers/TimeCircuitsViewController.swift
@@ -9,7 +9,13 @@
 import UIKit
 
 class TimeCircuitsViewController: UIViewController {
-
+    @IBOutlet var destinationTimeLabel: UILabel!
+    @IBOutlet var presentTimeLabel: UILabel!
+    
+    @IBOutlet var lastTimeDepartedLabel: UILabel!
+    
+    @IBOutlet var speedLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
