48089337b42a1126e6c096e391cbfce46d5c252a
Ian Becker <ian-becker@lambdastudents.com>
2020-05-20 11:34:12 -0700

    Started project

---
+++ OutaTime/OutaTime/TimeCircuitsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TimeCircuitsViewController.swift
+//  OutaTime
+//
+//  Created by Ian Becker on 5/20/20.
+//  Copyright © 2020 Ian Becker. All rights reserved.
+//
+
+import UIKit
+
+class TimeCircuitsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
