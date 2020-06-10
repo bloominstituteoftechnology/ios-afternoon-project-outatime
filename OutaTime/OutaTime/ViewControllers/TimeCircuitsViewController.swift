29d3dad9cc0c8cd7cb6a4f7552a25a7ce4f89dc6
Cora Jacobson <cora-jacobson@lambdastudents.com>
2020-06-09 20:53:36 -0700

    Additions

--- OutaTime/OutaTime/ViewControllers/TimeCircuitsViewController.swift
+++ OutaTime/OutaTime/ViewControllers/TimeCircuitsViewController.swift
@@ -14,6 +14,7 @@ class TimeCircuitsViewController: UIViewController {
     @IBOutlet weak var presentTimeLabel: UILabel!
     @IBOutlet weak var lastTimeDepartedLabel: UILabel!
     @IBOutlet weak var speedLabel: UILabel!
+    @IBOutlet weak var travelBackButton: UIButton!
     
     var dateFormatter: DateFormatter {
         let formatter = DateFormatter()
@@ -29,12 +30,14 @@ class TimeCircuitsViewController: UIViewController {
         presentTimeLabel.text = dateFormatter.string(from: Date())
         lastTimeDepartedLabel.text = "--- -- ----"
         speedLabel.text = "\(speed) MPH"
-        
+        travelBackButton.setTitleColor(UIColor.darkGray, for: .normal)
     }
     
     private var timer: Timer? = nil
     
     func startTimer() {
+        travelBackButton.setTitleColor(UIColor.darkGray, for: .normal)
+        travelBackButton.isEnabled = false
         timer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true, block: updateSpeed(timer:))
     }
         
@@ -49,6 +52,7 @@ class TimeCircuitsViewController: UIViewController {
             speedLabel.text = "\(speed) MPH"
         } else {
             resetTimer()
+            travelBackButton.isEnabled = true
             lastTimeDepartedLabel.text = presentTimeLabel.text
             presentTimeLabel.text = destinationTimeLabel.text
             destinationTimeLabel.text = "--- -- ----"
@@ -62,7 +66,14 @@ class TimeCircuitsViewController: UIViewController {
     }
     
     @IBAction func travelBackButton(_ sender: Any) {
-        startTimer()
+        if destinationTimeLabel.text != "--- -- ----" {
+            startTimer()
+        } else {
+            let alert = UIAlertController(title: "No Destination Time", message: "Please set a destination time.", preferredStyle: .alert)
+            let okButton = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            alert.addAction(okButton)
+            present(alert, animated: true, completion: nil)
+        }
     }
     
     // MARK: - Navigation
@@ -80,5 +91,6 @@ class TimeCircuitsViewController: UIViewController {
 extension TimeCircuitsViewController: DatePickerDelegate {
     func destinationDateWasChosen(_ date: Date) {
         destinationTimeLabel.text = dateFormatter.string(from: date)
+        travelBackButton.setTitleColor(UIColor(red:83/255, green:27/255, blue:147/255, alpha:1.0), for: .normal)
     }
 }
