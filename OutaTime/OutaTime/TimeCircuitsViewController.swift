40cf18d30c112f1e0b6bda932aefaec3a0726549
Brys1Saclaus2 <brys1saclaus2@gmail.com>
2020-06-10 18:30:18 -0700

    almost done

--- OutaTime/OutaTime/TimeCircuitsViewController.swift
+++ OutaTime/OutaTime/TimeCircuitsViewController.swift
@@ -20,8 +20,10 @@ class TimeCircuitsViewController: UIViewController {
     @IBOutlet weak var speedLabel: UILabel!
     
     
-    @IBAction func travelBackTapped(_ sender: Any) {
-    }
+    @IBOutlet weak var travelBacktTapped: UIButton!
+    
+    
+    
     
     private var dateformatter: DateFormatter = {
         let formatter = DateFormatter()
@@ -29,36 +31,78 @@ class TimeCircuitsViewController: UIViewController {
         return formatter
     }()
     
-    
-    var currentSpeed: Int = 0
+    var speed: Int = 0
     
     override func viewDidLoad() {
-        super.viewDidLoad()
+          super.viewDidLoad()
+        destinationTimeLabel.text = "--- -- ----"
+        presentTimeLabel.text = dateformatter.string(from: Date())
+        lastTimeDepartedLabel.text = "--- -- ----"
+        speedLabel.text = "\(speed) MPH"
+        
+        
+ var timer: Timer? = nil
+    
+    
+    func startTimer() {
+        timer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true, block: (updateSpeed))
+        
+    }
+        
+    func resetTimer() {
+            timer?.invalidate()
+            timer = nil
+        }
+        
+        func updateSpeed(timer: Timer) {
+            if speed < 88 {
+                speed += 1
+                speedLabel.text = "\(speed) MPH"
+            } else {
+                resetTimer()
+                travelBacktTapped.isEnabled = true
+                lastTimeDepartedLabel.text = presentTimeLabel.text
+                presentTimeLabel.text = destinationTimeLabel.text
+                destinationTimeLabel.text = "--- -- ----"
+                speed = 0
+                speedLabel.text = "\(speed) MPH"
+                let alert = UIAlertController(title: "Time Travel Successful", message: "You have arrived, the date is \(presentTimeLabel.text ?? "unknown")", preferredStyle: .alert)
+                let okButton = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                alert.addAction(okButton)
+                present(alert, animated: true, completion: nil)
+            }
+        }
 
         // Do any additional setup after loading the view.
         
         presentTimeLabel.text = dateformatter.string(from: Date())
-        speedLabel.text = "\(currentSpeed) MPH"
+        speedLabel.text = "\(speed) MPH"
         lastTimeDepartedLabel.text = "--- -- ----"
     }
     
   
 
-    /*
+   
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        if segue.identifier == "modalDestinationDatePickerSegue" {
+            if let datePickerVC = segue.destination as? DatePickerViewController {
+                datePickerVC.delegate = self
+            }
+        }
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
     }
-    */
+   
 
 }
 
 extension TimeCircuitsViewController: DatePickerDelegate {
     func destinationDateWasChosen(_ date: Date) {
-        <#code#>
+        destinationTimeLabel.text = dateformatter.string(from: date)
     }
     
     
